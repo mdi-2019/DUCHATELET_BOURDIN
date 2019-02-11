199a0729df770f79aaa6eb00afb0010dd00e28d4
corentinduchatelet <corentin.duchatelet@orange.fr>
2019-02-11 15:38:58 +0100

    Calculator mode This commit does the following: -User can input his mode of calculator by choosing 0 or 1

--- src/main/java/com/mdi/calculator/App.java
+++ src/main/java/com/mdi/calculator/App.java
@@ -1,13 +1,24 @@
 package com.mdi.calculator;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	enum Mode {STANDARD, SCIENTIFIC};
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Choisissez votre mode: \n\t0 - standard\n\t1 - scientific");
+        
+        Scanner scan = new Scanner(System.in);
+        int choice = scan.nextInt();
+        
+        Mode mode = (choice==0)?Mode.STANDARD:Mode.SCIENTIFIC;
+        
+        System.out.println(mode);
+        
     }
 }
