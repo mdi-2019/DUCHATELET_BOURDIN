4f5110817ff77a80631f6f048e8ee598e652b36d
Corentin Duchatelet <corentin.duchatelet@orange.fr>
2019-02-11 17:33:02 +0100

    Feature/calculator simple operation (#1)
    The calculator standard is implemented
    
    This commit does the following:
    -The file CalculatorStandard is implemented
    -When running, the user gives two values and chooses the operation to do to it (+,-,*,/)
    -The program stops when the user says "o" at the last question

--- src/main/java/com/mdi/calculator/CalculatorStandard.java
+++ src/main/java/com/mdi/calculator/CalculatorStandard.java
@@ -4,6 +4,56 @@ import java.util.Scanner;
 
 public class CalculatorStandard {
 	public CalculatorStandard(){
-		
+		Scanner scan = new Scanner(System.in);
+        int oldValue = 0;
+        while(true){
+        	System.out.println("\nQuelle valeur pour a?(Ans pour l'ancienne valeur)");
+        	String aChoice = scan.next();
+        	int a=1;
+        	if(aChoice.equals("Ans")){
+        		a = oldValue;
+        	}
+        	else {
+        		a = Integer.parseInt(aChoice);
+        	}
+        	
+        	
+
+        	System.out.println("\nQuelle valeur pour b?(Ans pour l'ancienne valeur)");
+        	String bChoice = scan.next();
+        	int b=1;
+        	if(bChoice.equals("Ans")){
+        		b = oldValue;
+        	}
+        	else {
+        		b = Integer.parseInt(bChoice);
+        	}
+
+
+            System.out.println("\nQuelle opération voulez-vous faire ?\n\t+,-,*,ou /");
+        	String opChoice = scan.next();
+        	
+        	switch(opChoice){
+	        	case "+":
+	        		oldValue = a + b;
+	        		break;
+	        	case "-":
+	        		oldValue = a - b;
+	        		break;
+	        	case "*":
+	        		oldValue = a * b;
+	        		break;
+	        	case "/":
+	        		if(b==0)b=1;
+	        		oldValue = a / b;
+	        		break;
+	        	default:
+	        		System.out.println("\nMauvaise opération");
+        	}
+        	System.out.println("RES : "+oldValue);
+        	
+        	System.out.println("\nStop? (o/n)\n\n");
+        	if(scan.next().equals("o")) break;
+        }
 	}
 }
